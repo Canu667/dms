255c1bc45b821defece8d29d3b3249b9e42068a6
Damian Gręda <damian.greda@icloud.com>
2018-07-29 16:36:35 +0200

    Added fronted app directory and basic skeleton. Modified grumphp configuration.

---
+++ frontend/webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path')
+const webpack = require('webpack');
+
+module.exports = {
+    context: __dirname,
+    entry: [
+        './src/index.jsx'
+    ],
+    devtool: 'cheap-eval-source-map',
+    output: {
+        path: path.join(__dirname, 'public'),
+        filename: 'bundle.js',
+        publicPath: '/public/'
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.json']
+    },
+    devServer: {
+        port: 9009,
+        host: '127.0.0.1',
+        disableHostCheck: true,
+        inline: true,
+        publicPath: '/public/',
+    },
+    module: {
+        /* it uses some kind of rule on your code */
+        rules: [
+            {
+                enforce: 'pre', /* ensures that it runs before babel */
+                test: /\.jsx?$/,
+                loader: 'eslint-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.jsx?$/,         // Match both .js and .jsx files
+                exclude: /node_modules/,
+                loader: "babel-loader",
+                query:
+                    {
+                        presets: ['react']
+                    }
+            }
+        ]
+    }
+}
