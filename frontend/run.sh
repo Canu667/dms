1cd8dab1895f0d6dfb5a65028de52477491e1235
Mateusz Canova <mcanova@notebooksbilliger.de>
2018-10-04 11:32:18 +0200

    Server test

--- frontend/run.sh
+++ frontend/run.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+sudo sed -i.bak "s/%SERVER%/$(curl http://169.254.169.254/latest/meta-data/public-hostname)/g" /var/app/current/frontend/public/index.html
 cd /home/node/app && ./node_modules/webpack-dev-server/bin/webpack-dev-server.js --config webpack.config.js --progress --content-base ./public --output-public-path /build/
